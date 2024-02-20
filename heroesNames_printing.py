bc6e770c7498c942b39596c4e310084cdb93e212
alokg-812 <154364202+alokg-812@users.noreply.github.com>
2024-02-20 19:26:09 +0530

    Heroes_names_printing_sequentially

---
+++ heroesNames_printing.py
@@ -0,0 +1,5 @@
+heroes = ['Iron-man','Thor','Cap. America', 'Hulk','Black Panther', 'Wanda', 'Spiderman', 'Ant-man', 'Captain Marvel' , 'Groot', 'Dr. Strange']
+i = 0
+while i< len(heroes) :
+    print(i+1,'.',heroes[i])
+    i += 1

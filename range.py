c45126198e6b7e1d144867c044d44ee01dc4d00b
alokg-812 <154364202+alokg-812@users.noreply.github.com>
2024-02-20 22:15:24 +0530

    range

---
+++ range.py
@@ -0,0 +1,16 @@
+# seq = range(5)
+# print(seq[0])
+# print(seq[1])
+# print(seq[2])
+# print(seq[3])
+# print(seq[4])
+
+# for i in range(5):
+#     print(i)
+
+# seq = range(10)
+# for i in seq:
+#     print(i+1)
+
+for i in range(2,100,2) :
+    print(i, end = '**')

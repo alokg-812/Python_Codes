50086e28b278bace3cb57cedfeaf3a4184c5eca2
alokg-812 <154364202+alokg-812@users.noreply.github.com>
2024-02-22 16:55:13 +0530

    square_printing

---
+++ squarePrinting.py
@@ -0,0 +1,13 @@
+# li = []
+# i = 0
+# while i < 10 :
+#     li[i] = i*i
+#     i+=1
+# print('li[]')
+
+li = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+index = 0
+while index < len(li) :
+    print(li[index] , end = " ")
+    index += 1
+

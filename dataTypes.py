5cb65307d015d43f43429fc8a3e4fbe1a080641d
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-11-18 16:39:15 +0530

    Delete dataTypes.py

--- dataTypes.py
+++
@@ -1,7 +0,0 @@
-name = "Alok"   #string
-age = 19        #int
-weight = 58.5   #float
-sex = "M"       #char
-relationship = None  #none
-
-print(type(name), type(age), type(weight), type(sex), type(relationship))

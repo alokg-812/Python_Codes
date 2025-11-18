7b50357a7e7887383fc80e868352ce8c7306376f
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-11-18 16:36:34 +0530

    Delete typeCasting.py

--- typeCasting.py
+++
@@ -1,7 +0,0 @@
-a = 1
-b = '2'
-type(b)
-c = int(b)
-print(c)
-print(type(c))
-#typecasting works only when the data which is going to be typecasted can be written in that form...ex "ALok" can not be typecasted into decimal or integer datatype hence it will throw error
